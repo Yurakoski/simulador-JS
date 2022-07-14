ecca38ea41a343beae5483a3011581b217647ade
Leandro Yurakoski <lyurakoski@hotmail.com>
2022-07-14 17:13:10 -0300

    arrays

--- js/main.js
+++ js/main.js
@@ -1,33 +1,44 @@
 let presupuesto = 4000;
-let valorJugador = 0;
-let jugadoresContratados = 0;
 let dineroGastado = 0;
 
-function ingresarMontoDeJugador(){
-    while (jugadoresContratados < 5){
-        valorJugador= parseInt(prompt("Ingrese el valor del jugador"));
-        validarPresupuesto(valorJugador);
-    }
-}
+const jugadoresContratados = [];
 
-function validarPresupuesto(valorJugador){
-    if (presupuesto >= valorJugador){
-        completarTransaccion(valorJugador);
+function validarCompra(jugador){
+    if (jugadoresContratados.length < 5){
+        if (presupuesto >= jugador.valor){
+             completarCompra(jugador);
+        }else{
+            alert("Su presupuesto es insuficiente. Contrate otro jugador más barato.");
+        }
     }
     else{
-        alert("Su presupuesto es insuficiente. Contrate otro jugador más barato.");
+        alert("Superó el límite de jugadores contratados");
     }
 }
 
-function completarTransaccion(valorJugador){
-    presupuesto -= valorJugador;
-    dineroGastado += valorJugador;
-    jugadoresContratados++;
+function completarCompra(jugador){
+    presupuesto -= jugador.valor;
+    dineroGastado += jugador.valor;
+    jugadoresContratados.push(jugador);
 }
 
-alert("Deberá contratar a 5 jugadores para su equipo de fútbol. Para ello contará con un presupuesto de $4000.");
-ingresarMontoDeJugador();
+function venderJugador(idJugador){
+    const index = jugadoresContratados.findIndex((jugador) => jugador.id === idJugador);
+    if(index !== -1){
+        presupuesto += jugadoresContratados[index].valor;
+        dineroGastado -=  jugadoresContratados[index].valor;
+        jugadoresContratados.splice(index, 1);
+    }
+}
+
+validarCompra({id: 1, nombre: "Mono burgos", valor: 800});
+validarCompra({id: 2, nombre: "Pupi Zanetti", valor: 500});
+validarCompra({id: 3, nombre: "Lionel Messi", valor: 1000});
+validarCompra({id: 4, nombre: "Brujita Verón", valor: 1000});
+validarCompra({id: 5, nombre: "Hernán Crespo", valor: 500});
+//validarCompra({id: 6, nombre: "Gabriel Batistuta", valor: 400});
+venderJugador(1);
 
 console.log("Dinero gastado: $ " + dineroGastado);
 console.log("Presupuesto disponible: $ " + presupuesto);
-console.log("Jugadores contratados: " + jugadoresContratados);
+console.log("Cantidad de jugadores contratados: " + jugadoresContratados.length);
