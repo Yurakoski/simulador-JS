19fb79cd52a1422fb1d438a5852da04c38d47b56
Leandro Yurakoski <lyurakoski@hotmail.com>
2022-08-21 18:00:24 -0300

    ruta jugadores json local

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ let jugadoresPlantel = [];
 cargarJugadores();
 
 function cargarJugadores(){
-    fetch('https://github.com/Yurakoski/simulador-JS/blob/main/js/jugadores.json')
+    fetch('/js/jugadores.json')
     .then((response) => response.json())
     .then((arrayJugadores) => {
         arrayJugadores.forEach((jugador)=>{agregarJugadorADisponibles(jugador)})
